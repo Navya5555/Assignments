a5faab2cdf04ab22cb4ba1e06a5e6bd8100aeaa8
Navya sri <navyasrij1497@gmail.com>
2024-01-26 09:30:09 -0500

    Navyas first commit message here

---
+++ datatype/datatype.java
@@ -0,0 +1,11 @@
+public class datatype {
+    public static void main(String[]args){
+            int a=30;
+            int b;
+            b= 20;
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+    }
+}
